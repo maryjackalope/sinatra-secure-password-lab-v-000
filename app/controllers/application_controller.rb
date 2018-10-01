9033f3578b3f980e3cfc9988fd397f7a45a1662c
Mary Jack <maryjackinthebox@gmail.com>
2018-10-01 19:58:06 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -39,6 +39,12 @@ class ApplicationController < Sinatra::Base
 
   post "/login" do
     ##your code here
+    user = User.fing_by(:username => params[:username])
+		if user && user.authenticate(params[:password])
+		  session[:user_id] = user.id
+		  redirect '/account'
+		else redirect "/failure"
+		end
   end
 
   get "/failure" do
